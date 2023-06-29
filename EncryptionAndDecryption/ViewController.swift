b26e96cdb9e40e9a8dde1c6776e15048ebeda121
alphonsa <alphonsanew22@gmail.com>
2023-06-29 11:55:37 +0530

    Initial Commit

---
+++ EncryptionAndDecryption/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EncryptionAndDecryption
+//
+//  Created by Alphonsa Varghese on 29/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
